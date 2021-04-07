a0825a289cd75b1896ede215aa28854f2dee2f14
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-04-07 17:21:54 +0200

    view port

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,8 @@ const config = require('../config.json');
   if (config && config.runHeadless) {
     launchOptions = {
       headless: true,
-      args: ['--no-sandbox'],
+      args: ['--no-sandbox', "--window-size=1920,1080"],
+      defaultViewport: null
     };
   }
 
@@ -19,6 +20,7 @@ const config = require('../config.json');
     var sitePromises = []
     for (let index = 0; index < sites.length; index += 1) {
       const page = await browser.newPage();
+      await page._client.send('Emulation.clearDeviceMetricsOverride');
       // making it wait for each loop on purpose to let previous chrome tab finish
       const pagePromise = checkSite(sites[index], page)
       pagePromise.then( () => {
