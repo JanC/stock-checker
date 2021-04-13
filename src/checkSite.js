dc02a512c04e4e815b28c272ab3225fb5adbf6fc
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-04-13 18:48:00 +0200

    waitUntil + save page

--- src/checkSite.js
+++ src/checkSite.js
@@ -1,5 +1,6 @@
 const { notify } = require('./notify');
 const config = require('../config.json');
+const fs = require('fs');
 
 function sleep(wait) {
   return new Promise((resolve) => setTimeout(resolve, wait * 1000));
@@ -15,24 +16,25 @@ const checkSite = async (site, page) => {
   console.log('[%s] Starting', description);
 
   await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-  await page.goto(url);
-  await sleep(wait);
+
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  });
   try {
 
     for (const xpath of clickXPaths) {
       const [button] = await page.$x(xpath);
       if (typeof button !== 'undefined') {
-        console.log('[%s] Clicking in %s', description, xpath);
         await button.click();
-        await sleep(wait);
       }
     }
 
+    await page.waitForXPath(xPath)
     const elHandle = await page.$x(xPath);
     const text = await page.evaluate((el) => el.textContent, elHandle[0]);
     const value = String(text).replace(/^\s+|\s+$/g, "");
     const match = isMatch(value, expected);
-    console.log('[%s] Out of stock element present %s', description, match);
+    console.log('[%s] Out of stock %s', description, match);
 
     if (!match) {
       await notify({
@@ -43,11 +45,14 @@ const checkSite = async (site, page) => {
   } catch (e) {
     console.error('[%s] Exception %s', description, e);
 
+    const html = await page.content();
+    await fs.promises.writeFile(description + '_page.html', html, { encoding: 'utf8' });
+    await page.screenshot({ path: description + '_page.png' });
+
     if (config && config.notifyOnNodeNotFound) {
       await notify({ site, message: `${description} could not reach the node specified` });
     }
   } finally {
-    console.log('[%s] Done', description);
   }
 };
 
