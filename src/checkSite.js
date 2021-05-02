880e46ab8940b00681f332c44c42915c24e79c4a
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2021-05-02 09:11:22 +0200

    Added multiple xpaths to a single site

--- src/checkSite.js
+++ src/checkSite.js
@@ -11,37 +11,70 @@ const isMatch = (actual, expected) => {
   return actual === expected;
 };
 
-const checkSite = async (site, page) => {
-  const { url, xPath, expected, wait = 1, description, clickXPaths = [] } = site;
-  console.log('[%s] Starting', description);
-
-  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-
-  await page.goto(url, {
-    waitUntil: 'networkidle2'
-  });
-  try {
-
-    for (const xpath of clickXPaths) {
-      const [button] = await page.$x(xpath);
-      if (typeof button !== 'undefined') {
-        await button.click();
-      }
+const waitForClicks = async (page, clickXPaths) => {
+  for (const xpath of clickXPaths) {
+    const [button] = await page.$x(xpath);
+    if (typeof button !== 'undefined') {
+      await button.click();
     }
+  }
+}
 
-    await page.waitForXPath(xPath)
+const checkXPath = async (page, xPath, expected, wait, description) => {
+  try {
+    await page.waitForXPath(xPath, {timeout: wait * 1000});
     const elHandle = await page.$x(xPath);
     const text = await page.evaluate((el) => el.textContent, elHandle[0]);
     const value = String(text).replace(/^\s+|\s+$/g, "");
     const match = isMatch(value, expected);
-    console.log('[%s] Out of stock %s', description, match);
+    console.log("[%s] Out of stock %s", description, match);
+    return match
+  } catch (e) {
+    console.error("[%s] Exception %s", description, e);
+    return false
+  }
+}
 
-    if (!match) {
+const openSite = async (page, site) => {
+  const { url, checks, wait = 1, description, clickXPaths = [] } = site;
+  console.log("[%s] Starting", description);
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  });
+}
+
+const checkPage = async (page, site) => {
+  const { url, checks, wait = 1, description, clickXPaths = [] } = site;
+  var anyMatch = false;
+  for (const { xPath, expected } of checks) {
+    console.log("[%s] Checking xpath %s", description, xPath);
+    const match = await checkXPath(page, xPath, expected, wait, description)
+    anyMatch = match & anyMatch
+    if (match) { break; }
+  }
+  return anyMatch
+}
+
+const checkSite = async (site, page) => {
+  const { url, checks, wait = 1, description, clickXPaths = [] } = site;
+
+  await openSite(page, site)
+  try {
+    await waitForClicks(page, clickXPaths)
+    const anyMatch = await checkPage(page, site)
+
+    if (!anyMatch) {
       await notify({
         site,
-        message: `${description} was expecting "${expected}" but got "${value}"`,
+        message: `${description} did not find any of the matching values"`,
       });
     }
+
+    
   } catch (e) {
     console.error('[%s] Exception %s', description, e);
 
